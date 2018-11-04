baf2836dc676bcf3173c100cf7eeb7dee3a539d0
Karin Ohman <kaohma@gmail.com>
2018-11-03 21:10:47 -0600

    Update show button functionality to remove cards from DOM

--- main.js
+++ main.js
@@ -2,54 +2,12 @@
 // URL.revokeObjectURL()
 
 var addToAlbumButton = document.getElementById('js-add-to-album');
+var cardSection = document.querySelector('.card-section');
+var chooseFileButton = document.getElementById('photo-input');
+var showButton = document.querySelector('.show-button');
 var textInputs = document.querySelectorAll('.text-inputs');
 var viewFavorites = document.getElementById('js-view-favorites');
-var chooseFileButton = document.getElementById('photo-input');
 var photosArray = [];
-var cardSection = document.querySelector('.card-section');
-
-viewFavorites.addEventListener('click', showFavorites);
-cardSection.addEventListener('click', event => {
-  if (event.target.classList.contains('delete-button')) {
-    deleteCard(event);
-  } else if (event.target.classList.contains('favorite-button')) {
-    favoriteCard(event);
-  }
-});
-
-function showFavorites() {
-  var allCards = Array.from(document.querySelectorAll('.photo-card'));
-
-  debugger
-  allCards.forEach(card => {
-    cardTarget = card.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.nextElementSibling;
-    if (cardTarget.classList.contains('favorite-button-active')) {
-      cardTarget.classList.remove('hidden');
-    } else {
-      cardTarget.classList.add('hidden');
-    }
-  });
-}
-
-function checkForFavorite(photoObj, cardTarget) {
-  if (photoObj.favorite === true) {
-    favoriteCounter++
-    cardTarget.classList.add('favorite-button-active');
-  } else {
-    if (favoriteCounter > 0) { favoriteCounter-- };
-    cardTarget.classList.remove('favorite-button-active');
-  }
-  document.getElementById('js-favorite-counter').innerText = favoriteCounter;
-}
-function favoriteCard() {
-  var cardTarget = event.target;
-  var objectId = cardTarget.parentElement.parentElement.dataset.id;
-  var index = findIndexNumber(objectId);
-  photosArray[index].updateFavorite();
-  photosArray[index].saveToStorage(photosArray);
-  checkForFavorite(photosArray[index], cardTarget);
-}
-
 
 // FIND A WAY TO KEEP THESE LOCAL!!!
 var photoURL; 
@@ -61,18 +19,13 @@ cardSection.addEventListener('dblclick', updateCard);
 addToAlbumButton.addEventListener('click', function() {
   saveNewPhotoCard(textInputs[0].value, textInputs[1].value, photoURL);
 });
-
-// Fix interaction with search input and show more button
 document.getElementById('js-search').addEventListener('keyup', liveSearch);
-document.querySelector('.show-button').addEventListener('click', showCards);
-
+showButton.addEventListener('click', showCards);
 
 
 function addCardPlaceholder() {
   document.querySelector('.card-placeholder').classList.remove('hide-placeholder');
 }
-
-
 
 function checkTextFields() {
   if (textInputs[0].value && textInputs[1].value) {
@@ -98,19 +51,21 @@ function convertPhotoFile(photo) {
   return window.URL.createObjectURL(photo);
 }
 
-function createCard(photo) {
-  var cardHTML = `
-    <div class="photo-card" data-id=${photo.id}>
-      <p class="js-text title">${photo.title}</p>
-      <img class="photo" src="${photo.file}" alt="use uploaded photo">
-      <p class="js-text caption">${photo.caption}</p>
-      <section class="card-footer">
-        <button class="icon-buttons delete-button"></button>
-        <button class="icon-buttons favorite-button"></button>
-      </section>
-    </div>`;
-  var cardSection = document.querySelector('.card-section');
-  cardSection.insertAdjacentHTML('afterbegin', cardHTML);
+function createCards(array) {
+  array.reverse()
+  for (var i = 0; i < array.length; i++) {
+    var cardHTML = `
+      <div class="photo-card" data-id=${array[i].id}>
+        <p class="js-text title">${array[i].title}</p>
+        <img class="photo" src="${array[i].file}" alt="user uploaded photo">
+        <p class="js-text caption">${array[i].caption}</p>
+        <section class="card-footer">
+          <button class="icon-buttons delete-button"></button>
+          <button class="icon-buttons favorite-button"></button>
+        </section>
+      </div>`;
+    cardSection.insertAdjacentHTML('afterbegin', cardHTML);
+  }
 }
 
 function createCardsOnReload() {
@@ -119,9 +74,8 @@ function createCardsOnReload() {
     parsedCard.forEach(function(object) {
       var photoObj = new Photo(object.title, object.caption, object.file, object.favorite, object.id);
       photosArray.push(photoObj);
-      createCard(photoObj);
       var cardTarget = findFavoriteTarget(photoObj.id);
-      checkForFavorite(photoObj, cardTarget);
+      // checkForFavorite(photoObj, cardTarget);
     });
     removeCardPlaceholder();
     showRecentCards();
@@ -132,6 +86,7 @@ function createCardsOnReload() {
 
 function deleteCard() {
   var objectId = event.target.parentElement.parentElement.dataset.id;
+  debugger
   var index = findIndexNumber(objectId);
   photosArray[index].deleteFromStorage(index);
   photosArray.splice(index, 1);
@@ -173,12 +128,26 @@ function findIndexNumber(objId) {
   }
 }
 
+function liveSearch() {
+  removeAllCards();
+  var searchInput = this.value;
+  if (searchInput === '') {
+    showRecentCards();
+    return
+  }
+  var shownArray = searchObjectText(photosArray, searchInput);
+  shownArray = sortShownArray(shownArray);
+  createCards(shownArray);  
+}
+
 function getPhoto(event) {
   photoURL = convertPhotoFile(event.target.files[0]);
   testTextFields();
 }
 
-
+function removeAllCards() {
+  cardSection.innerHTML = '';
+}
 
 function removeCardPlaceholder() {
   document.querySelector('.card-placeholder').classList.add('hide-placeholder');
@@ -192,7 +161,6 @@ function removeListeners() {
 function saveNewPhotoCard(title, caption, photoUrl) {
   event.preventDefault();
   var photoObj = new Photo(title, caption, photoUrl);
-  createCard(photoObj);
   photosArray.push(photoObj);
   photoObj.saveToStorage(photosArray);
   disableButton(addToAlbumButton);
@@ -218,6 +186,16 @@ function saveTextOnEnter(event) {
   }
 }
 
+function searchObjectText(array, input) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if ((array[i].title.toLowerCase().indexOf(input) != -1) || (array[i].caption.toLowerCase().indexOf(input) != -1)) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray
+}
+
 function setEditable() {
   event.target.contentEditable = true;
 }
@@ -226,10 +204,58 @@ function setUneditable() {
   event.target.contentEditable = false;
 }
 
+function showCards() {
+  removeAllCards();
+  var showMore = toggleShowButton();
+  if (showMore) {
+    var showAll = photosArray.slice().reverse();
+    createCards(showAll);
+  } else {
+    showRecentCards();
+  }
+}
+
+function showRecentCards() {
+  removeAllCards();
+  if (photosArray.length < 10) {
+    var showAll = photosArray.slice().reverse();
+    createCards(showAll);
+    disableButton(showButton);
+  } else {
+    enableButton(showButton);
+    var shownArray = photosArray.slice(0, 10); 
+    createCards(shownArray);
+  }
+}
+
+function sortShownArray(array) {
+  array.sort(function(a, b) {
+    var idA = a.id;
+    var idB = b.id;
+    if (idA < idB) {
+      return -1;
+    }
+    if (idA > idB) {
+      return 1;
+    }
+  });
+  return array
+}
+
 function testTextFields() {
   checkTextFields();
   textInputs[0].addEventListener('input', checkTextFields);
   textInputs[1].addEventListener('input', checkTextFields);
+}
+
+function toggleShowButton() {
+  if (showButton.innerText === 'Show More...') {
+    showButton.innerText = 'Show Less...';
+    return true
+  } else {
+    showButton.innerText = 'Show More...';
+    return false
+  }
 }
 
 function updateCard() {
@@ -250,101 +276,63 @@ function updateObject() {
   photosArray[index].saveToStorage(photosArray);
 }
 
-
-
-
-
-
 // function to get unique IDs from cards using event target
-function getCardID(target) {
-  return objId = target.dataset.id;
-}
+// function getCardID(target) {
+//   return objId = target.dataset.id;
+// }
 
-// function to get array index using unique ID
-function getArrayIndex(array, objId) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i].id === objId) {
-      return i
-    }
+// // function to get array index using unique ID
+// function getArrayIndex(array, objId) {
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i].id === objId) {
+//       return i
+//     }
+//   }
+// }
+
+// viewFavorites.addEventListener('click', showFavorites);
+cardSection.addEventListener('click', event => {
+  if (event.target.classList.contains('delete-button')) {
+    deleteCard(event);
+  } else if (event.target.classList.contains('favorite-button')) {
+    favoriteCard(event);
   }
-}
+});
 
-// when you filter, delete cards you don't need from the DOM but keep in array/storage
-// as you back out of filter, create cards again by accessing cards that should be shown again in the array
-// so search/filter really needs to check the text inside each object instance, not in the HTML.
-// shown array is constantly updated but photosArray holds all instances
+// function showFavorites() {
+//   var allCards = Array.from(document.querySelectorAll('.photo-card'));
 
+//   debugger
+//   allCards.forEach(card => {
+//     cardTarget = card.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.firstElementChild.nextElementSibling;
+//     if (cardTarget.classList.contains('favorite-button-active')) {
+//       cardTarget.classList.remove('hidden');
+//     } else {
+//       cardTarget.classList.add('hidden');
+//     }
+//   });
+// }
 
-// Search/filter functionality
-function searchObjectText(array, input) {
-  var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if ((array[i].title.toLowerCase().indexOf(input) != -1) || (array[i].caption.toLowerCase().indexOf(input) != -1)) {
-      newArray.push(array[i]);
-    }
-  }
-  return newArray
-}
-
-function liveSearch() {
-  removeAllCards();
-  var searchInput = this.value;
-  var shownArray = searchObjectText(photosArray, searchInput);
-  shownArray = sortShownArray(shownArray);
-  for (var i = 0; i < shownArray.length; i++) {
-    createCard(shownArray[i]);
-  };   
-};
-
-function sortShownArray(array) {
-  array.sort(function(a, b) {
-    var idA = a.id;
-    var idB = b.id;
-    if (idA < idB) {
-      return -1;
-    }
-    if (idA > idB) {
-      return 1;
-    }
-  });
-  return array
-}
-
-function removeAllCards() {
-  cardSection.innerHTML = '';
-}
-
-
-// Show cards functionality
-function showCards() {
-  var allCards = Array.from(document.querySelectorAll('.photo-card'));
-  if (event.target.innerText === 'Show More...') {
-    event.target.innerText = 'Show Less...';
-    allCards.forEach((card, i) => {
-      card.classList.remove('hidden');
-    });
+function checkForFavorite(photoObj, cardTarget) {
+  if (photoObj.favorite === true) {
+    favoriteCounter++
+    cardTarget.classList.add('favorite-button-active');
   } else {
-    event.target.innerText = 'Show More...';
-    showRecentCards();
+    if (favoriteCounter > 0) { favoriteCounter-- };
+    cardTarget.classList.remove('favorite-button-active');
   }
+  document.getElementById('js-favorite-counter').innerText = favoriteCounter;
 }
 
-function showRecentCards() {
-  var allCards = Array.from(document.querySelectorAll('.photo-card'));
-  var showButton = document.querySelector('.show-button');
-  if (photosArray.length >= 10) {
-    enableButton(showButton);
-    allCards.forEach((card, i) => {
-      if (i < 10) { 
-        card.classList.remove('hidden');
-      } else {
-        card.classList.add('hidden');
-      }
-    });  
-  } else {
-    disableButton(showButton);
-  }
+function favoriteCard() {
+  var cardTarget = event.target;
+  var objectId = cardTarget.parentElement.parentElement.dataset.id;
+  var index = findIndexNumber(objectId);
+  photosArray[index].updateFavorite();
+  photosArray[index].saveToStorage(photosArray);
+  checkForFavorite(photosArray[index], cardTarget);
 }
+
 
 
 
